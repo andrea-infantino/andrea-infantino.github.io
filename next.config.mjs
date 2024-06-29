aff673576f96dca7e8af4e44287e36e43e3eb6c1
InfantinoAndrea00 <andrea1.infantino@mail.polimi.it>
2024-06-29 14:18:22 +0200

    Bugfixing

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,13 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
 const nextConfig = {
     output: 'export',
     images: {
         unoptimized: true,
     },
-    basePath: '',
+    assetPrefix: isProd ? '/website/' : '',
+    basePath: isProd ? '/website' : '',
+    trailingSlash: true,
 };
 
 export default nextConfig;
