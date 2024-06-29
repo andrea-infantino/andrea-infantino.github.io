267f68fdd02bb5f6217f187b9e5ef2012ad0af43
andrea-infantino <andrea-infantino@users.noreply.github.com>
2024-06-29 14:50:42 +0000

    deploy: 72e6db73d8e00c597c2afd7001e4ba2a363141ca

---
+++ _next/static/chunks/app/page-fa6786f9049342f4.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[931],{8e3:function(e,n,t){Promise.resolve().then(t.t.bind(t,8893,23))},8893:function(e){e.exports={container:"page_container__p0I5Z",title:"page_title__yIZj8",description:"page_description__Aqec2"}}},function(e){e.O(0,[222,971,23,744],function(){return e(e.s=8e3)}),_N_E=e.O()}]);
