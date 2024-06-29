72e6db73d8e00c597c2afd7001e4ba2a363141ca
InfantinoAndrea00 <andrea1.infantino@mail.polimi.it>
2024-06-29 16:50:02 +0200

    Implemented NavBar

---
+++ components/Layout.tsx
@@ -0,0 +1,12 @@
+import React, { ReactNode } from 'react';
+import styles from '../styles/Layout.module.css';
+
+interface LayoutProps {
+  children: ReactNode;
+}
+
+const Layout: React.FC<LayoutProps> = ({ children }) => {
+  return <div className={styles.container}>{children}</div>;
+};
+
+export default Layout;
---
+++ components/Navbar.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import React, {useState} from 'react';
+import Link from 'next/link';
+import styles from '../styles/Navbar.module.css';
+
+const Navbar: React.FC = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+      setIsOpen(!isOpen);
+  };
+
+  return (
+    <nav className={styles.navbar}>
+      <Link href="/" className={styles.navbarLeft}>
+        <div className={styles.profileImageContainer}>
+          <img src="/images/profile.jpg" alt="Profile" className={styles.profileImage} />
+        </div>
+        <div className={styles.navbarTitle}>ANDREA INFANTINO</div>
+      </Link>
+      <div className={`${styles.navbarMenu} ${isOpen ? styles.open : ''}`}>
+        <Link href="/" className={styles.navbarItem}>Home</Link>
+        <Link href="/about" className={styles.navbarItem}>About</Link>
+        <Link href="/services" className={styles.navbarItem}>Services</Link>
+        <Link href="/contact" className={styles.navbarItem}>Contact</Link>
+      </div>
+      <div className={styles.navbarToggle} onClick={toggleMenu}>
+        <span className={styles.bar}></span>
+        <span className={styles.bar}></span>
+        <span className={styles.bar}></span>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
